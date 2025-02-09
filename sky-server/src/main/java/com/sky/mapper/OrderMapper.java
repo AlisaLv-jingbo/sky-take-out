654940e2bc04f2ca0eb2ef6a0856bb838ef24211
lvjingbo_win <1164984412@qq.com>
2025-02-09 17:35:54 +0800

    微信小程序 支付操作

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,8 +2,18 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface OrderMapper {
     void insert(Orders orders);
+
+    /**
+     * 根据订单号查询订单
+     * @param orderNumber
+     */
+    @Select("select * from sky_take_out.orders where number = #{orderNumber}")
+    Orders getByNumber(String orderNumber);
+
+    void update(Orders orders);
 }
