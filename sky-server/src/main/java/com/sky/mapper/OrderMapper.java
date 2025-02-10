ca045b2830416fc09cbd2c6a5ce3390dafd5ab68
lvjingbo_win <1164984412@qq.com>
2025-02-10 12:17:11 +0800

    微信小程序 支付操作

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -16,4 +18,12 @@ public interface OrderMapper {
     Orders getByNumber(String orderNumber);
 
     void update(Orders orders);
+
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    @Select("select * from sky_take_out.orders where id = #{id}")
+    Orders getById(Long id);
+
+    @Select("select count(id) from sky_take_out.orders where status = #{status}")
+    Integer countStatus(Integer toBeConfirmed);
 }
